4d65d6b3e067a096943c0cf41d1eaf199a32abf2
JackeviciusR <rokas.jackevicius@gmail.com>
2020-12-18 19:09:08 +0200

    Sinchronised timer #17

--- js/components/counter/Counter.js
+++ js/components/counter/Counter.js
@@ -6,7 +6,7 @@ class Counter {
         this.DOM = null;
         this.countersDOMs = null;
 
-        this.animationDuration = 5;
+        this.animationDuration = 3;
         this.animationFPS = 30;
 
         this.init();
@@ -19,6 +19,9 @@ class Counter {
 
         this.render();
         this.addEvents();
+
+        // rodytu, jei langas atsidarytu tiesiai ant COUNTER sekcijos ir neskrolintume
+        this.shouldCounterAnimationRun();
     }
 
     isValidSelector() {
@@ -37,7 +40,7 @@ class Counter {
 
         for(let item of this.data) {
             HTML += `<div class="counter col-3 col-sm-6 col-xs-12">
-                        <div class="number">${item.number}${item.type}</div>
+                        <div class="number">0${item.number}${item.type}</div>
                         <div class="label">${item.label}</div>
                     </div>`;
         }
@@ -46,42 +49,58 @@ class Counter {
         this.countersDOMs = this.DOM.querySelectorAll('.counter');
     }
 
-    counterAnimation(counterIndex) {
-        let count = 0;
 
+    counterAnimation(counterIndex) {
+
+        const targetNumber = this.data[counterIndex].number;
+        const totalTickCount = this.animationFPS * this.animationDuration;
         const numberDOM = this.countersDOMs[counterIndex].querySelector('.number');
+        let count = 0;
+        let tick = 0;
 
         const timer = setInterval(() => {
-            numberDOM.innerText = count++ + this.data[counterIndex].type;
+            count = Math.floor(tick / totalTickCount * targetNumber);
+            tick++;
 
-            if (this.data[counterIndex].number === count) {
+            numberDOM.innerText = count + this.data[counterIndex].type;
+
+
+            if (targetNumber === count) {
                 clearInterval(timer);
             }
-        }, 1000 / this.animationFPS)
+        }, 1000 / this.animationFPS);
     }
 
-    
-    addEvents() {
-        console.log(this.countersDOMs);
 
-        addEventListener('scroll', () => {
-            const windowBottom = scrollY + innerHeight;
-            let counterBottom = 0;
+    shouldCounterAnimationRun() {
+        const windowBottom = scrollY + innerHeight;
+        let counterBottom = 0;
 
-            for (let i = 0; i < this.countersDOMs.length; i++) {
-                if (this.data[i].animated) {
-                    continue;
-                }
-
-                const counter = this.countersDOMs[i];
-                counterBottom = counter.clientHeight + counter.offsetTop;
-
-                if (counterBottom < windowBottom) {
-                    this.data[i].animated = true;
-                    this.counterAnimation(i);
-                }
+        for (let i = 0; i < this.countersDOMs.length; i++) {
+            if (this.data[i].animated) {
+                continue;
             }
-        })
+
+            const counter = this.countersDOMs[i];
+                       // pacio elemento aukstis + kokiame aukstyje elementas yras
+            counterBottom = counter.clientHeight + counter.offsetTop;
+
+            if (counterBottom < windowBottom) {
+                // priskiriame nauja objekto kintamaji animated su reiksme true
+                this.data[i].animated = true;
+                this.counterAnimation(i);
+            }
+        }
+    }
+
+
+
+    addEvents() {
+        console.log('countersDOMs: ', this.countersDOMs);
+
+        // su bind nurodome, kad funkcija imama ne is scroll'o ('this') objekto, bet
+        // is 'this' objekto, kuriame viskas ir yra aprasya (t.y Counter objekto);
+        addEventListener('scroll', this.shouldCounterAnimationRun.bind(this));
     }
 
 }
